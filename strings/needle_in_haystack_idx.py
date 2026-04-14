a346ca996af01b54e82c03d6a6c81438451a330a
Aditya Ajay Phalod <aphalod@student.ubc.ca>
2026-04-14 13:39:48 -0700

    Add strStr method to find substring index
    Implement strStr function to find the first occurrence of a substring.

---
+++ strings/needle_in_haystack_idx.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+
+        if needle in haystack:
+            i = haystack.find(needle)
+            # print(i)
+            return i
+
+        else:
+            return -1
